7f77359819d9051ee9a7921463ea0b61f6084edc
ingAndresMartinez <ing.andresmartinezcobos@gmail.com>
2020-04-05 14:47:02 -0500

    Add Java project

---
+++ src/main/java/co/edu/javeriana/eas/patterns/persistence/JavEasPatternsPersistenceLibApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.patterns.persistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavEasPatternsPersistenceLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavEasPatternsPersistenceLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/eas/patterns/persistence/JavEasPatternsPersistenceLibApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.patterns.persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavEasPatternsPersistenceLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
