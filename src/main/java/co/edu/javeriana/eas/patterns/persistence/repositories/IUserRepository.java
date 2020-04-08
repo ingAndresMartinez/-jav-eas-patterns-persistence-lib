25b320b57c0bcc2fab8aa59a897dccd3609efb4b
ingAndresMartinez <ing.andresmartinezcobos@gmail.com>
2020-04-08 15:10:35 -0500

    Update status user

--- src/main/java/co/edu/javeriana/eas/patterns/persistence/repositories/IUserRepository.java
+++ src/main/java/co/edu/javeriana/eas/patterns/persistence/repositories/IUserRepository.java
@@ -3,7 +3,11 @@ package co.edu.javeriana.eas.patterns.persistence.repositories;
 import co.edu.javeriana.eas.patterns.persistence.entities.ProfileEntity;
 import co.edu.javeriana.eas.patterns.persistence.entities.UserEntity;
 import co.edu.javeriana.eas.patterns.persistence.entities.UserStatusEntity;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +19,10 @@ public interface IUserRepository extends CrudRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUserCodeAndPassword(String userCode, String password);
 
     List<UserEntity> findByStatus(UserStatusEntity status);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update usuario set id_estado =:status where id_usuario =:id", nativeQuery = true)
+    void updateStatus(@Param("status") int status, @Param("id") int id);
 
 }
